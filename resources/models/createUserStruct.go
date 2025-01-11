c820eb3607df8ecfe25da93b1c72be7c88e9e9df
megaredsss <58664975+megaredsss@users.noreply.github.com>
2025-01-11 23:57:45 +0300

    Add comments to model package

--- resources/models/createUserStruct.go
+++ resources/models/createUserStruct.go
@@ -1,5 +1,6 @@
 package models
 
+// Struct for creating user
 type CreateUserStruct struct {
 	FirstName  string
 	SecondName string
--- resources/models/userStruct.go
+++ resources/models/userStruct.go
@@ -1,5 +1,6 @@
 package models
 
+// User struct for database
 type User struct {
 	ID         uint   `gorm:"primaryKey"`
 	FirstName  string `gorm:"<-"`
